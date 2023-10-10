4080d9b5ac8539ae72e7be440028546ca3f6be5c
manggala56 <manggalaputra740@gmail.com>
2023-10-10 21:09:19 +0700

    new config

--- application/views/admin/leafletpoint/mapView.php
+++ application/views/admin/leafletpoint/mapView.php
@@ -1,4 +1,7 @@
 <?=content_open($title)?>
-<div class="filter">test</div>
+<div class="mb-3" id="date_input">
+    <label for="date_filter" class="form-label">filter date</label>
+    <input type="date" class="form-control" id="date_filter">
+  </div>
  	<div id="map"></div>
 <?=content_close()?>
