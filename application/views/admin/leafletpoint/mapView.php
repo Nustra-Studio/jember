2b7d41fad5c3de8aa112987514a3940c4dfdf513
manggala56 <manggalaputra740@gmail.com>
2023-10-10 21:19:21 +0700

    new input filte

--- application/views/admin/leafletpoint/mapView.php
+++ application/views/admin/leafletpoint/mapView.php
@@ -1,10 +1,7 @@
 <?=content_open($title)?>
 <div class="mb-3" id="date_input">
-<label for="monthInput">Select a month:</label>
-<input type="month" id="monthInput" name="month">
-<br><br>
-<label for="dateInput">Select a date:</label>
-<input type="date" id="dateInput" name="date">
+    <label for="date_filter" class="form-label">filter date</label>
+    <input type="month" class="form-control" id="date_filter">
 	<button type="button" class="btn btn-primary mt-2">search</button>
   </div>
  	<div id="map"></div>
