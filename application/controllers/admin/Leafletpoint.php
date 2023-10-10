781a1edd00ba070962496b3f8d8227aeae83aeae
manggala56 <manggalaputra740@gmail.com>
2023-10-10 22:52:50 +0700

    new fitur filter

--- application/controllers/admin/Leafletpoint.php
+++ application/controllers/admin/Leafletpoint.php
@@ -18,11 +18,13 @@ class Leafletpoint extends CI_Controller {
 		$datacontent['url_kecamatan']="admin/api/data/kecamatan?date=$date";
 		$datacontent['date']="$date";
 		}
-		$datacontent['url_kategorihotspot']="admin/api/data/kategorihotspot";
-		$datacontent['url_kecamatan']="admin/api/data/kecamatan";
-		$datacontent['url']="admin/leafletpoint";
-		$datacontent['date']="";
-		$datacontent['title']="Peta Kunjungan Pasien";
+		else{
+			$datacontent['url_kategorihotspot']="admin/api/data/kategorihotspot";
+			$datacontent['url_kecamatan']="admin/api/data/kecamatan";
+			$datacontent['url']="admin/leafletpoint";
+			$datacontent['date']="";
+			$datacontent['title']="Peta Kunjungan Pasien";
+		}
 		// $datacontent['datatable']=$this->Model->get();
 		$data['content']=$this->load->view('admin/leafletpoint/mapView',$datacontent,TRUE);
 		$data['js']=$this->load->view('admin/leafletpoint/js/mapJs',$datacontent,TRUE);
