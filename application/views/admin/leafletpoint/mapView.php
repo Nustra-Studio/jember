ce96585f703656aadd6f90dfa800db91b669e822
manggala56 <manggalaputra740@gmail.com>
2023-10-10 21:11:13 +0700

    new input filte

--- application/views/admin/leafletpoint/mapView.php
+++ application/views/admin/leafletpoint/mapView.php
@@ -2,7 +2,7 @@
 <div class="mb-3" id="date_input">
     <label for="date_filter" class="form-label">filter date</label>
     <input type="date" class="form-control" id="date_filter">
-	<button type="button" class="btn btn-primary">search</button>
+	<button type="button" class="btn btn-primary mt-2">search</button>
   </div>
  	<div id="map"></div>
 <?=content_close()?>
